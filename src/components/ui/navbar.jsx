f02371d6f9d16dbd0d4cd990e1b4c1d2617e2953
Lance <poplance88@gmail.com>
2025-12-16 17:00:06 +0800

    chore(portfoilo): make it friendly for guthub pages

--- src/components/ui/navbar.jsx
+++ src/components/ui/navbar.jsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { Button } from "@/components/ui/button"
+import { Link } from "react-router-dom"
 
 export default function Navbar() {
     return (
@@ -7,7 +8,7 @@ export default function Navbar() {
             <div className="relative h-16 sm:h-20 w-full overflow-hidden border-b border-white/10">
                 {/* Background image */}
                 <img
-                    src="/images/Galaxy_nav.jpg"
+                    src={`${import.meta.env.BASE_URL}images/Galaxy_nav.jpg`}
                     alt="Galaxy background"
                     aria-hidden="true"
                     className="absolute inset-0 h-full w-full object-cover contrast-[2.4] brightness-[0.65]"
@@ -25,7 +26,7 @@ export default function Navbar() {
                             className="text-white/90 hover:text-white hover:bg-white/10"
                             asChild
                         >
-                            <a href="/">Home</a>
+                            <Link to="/">Home</Link>
                         </Button>
                     </nav>
 
@@ -43,7 +44,8 @@ export default function Navbar() {
                             className="text-white/90 hover:text-white hover:bg-white/10"
                             asChild
                         >
-                            <a href="#about">About Me</a>
+                            {/* If your About is on Home page as an anchor, this is simplest */}
+                            <a href={`${import.meta.env.BASE_URL}#about`}>About Me</a>
                         </Button>
                     </nav>
                 </div>
